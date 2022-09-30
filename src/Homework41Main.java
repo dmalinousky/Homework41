878a7c3dc361911011d18bc2227b64d47ba87bae
Daniil Malinousky <103819784+dmalinousky@users.noreply.github.com>
2022-09-30 21:36:42 +0300

    Initial commit

---
+++ src/Homework41Main.java
@@ -0,0 +1,5 @@
+public class Homework41Main {
+    public static void main(String[] args) {
+        System.out.println("Please, do not give this homework score until 03/10/2022. Thanks.");
+    }
+}
